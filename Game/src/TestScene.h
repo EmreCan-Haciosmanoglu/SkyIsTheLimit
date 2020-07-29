1b6973b4799e16222fb9d5702c2c6235f5fe7a6e
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2020-07-29 20:17:14 +0300

    Road, Junction and End classes are moved to their own classes

--- Game/src/TestScene.h
+++ Game/src/TestScene.h
@@ -1,90 +1,12 @@
 #pragma once
 #include "Can.h"
 
+#include "Road.h"
+#include "Junction.h"
+#include "End.h"
+
 namespace Can
 {
-	struct Road;
-	struct Junction;
-	struct End;
-
-	struct Road {
-		glm::vec3 startPos;
-		glm::vec3 endPos;
-		Can::Object* object = nullptr;
-		Junction* startJunction = nullptr;
-		Junction* endJunction = nullptr;
-		End* startEnd = nullptr;
-		End* endEnd = nullptr;
-	};
-	struct sort_with_angle
-	{
-		inline bool operator() (const Road* road1, const Road* road2)
-		{
-			glm::vec3 R0R1_1;
-			glm::vec3 R0R1_2;
-			if (
-				road1->endJunction != nullptr &&
-				road2->endJunction != nullptr &&
-				road1->endJunction == road2->endJunction
-				)
-			{
-				R0R1_1 = road1->startPos - road1->endPos;
-				R0R1_2 = road2->startPos - road2->endPos;
-			}
-			else if (
-				road1->endJunction != nullptr &&
-				road2->startJunction != nullptr &&
-				road1->endJunction == road2->startJunction
-				)
-			{
-				R0R1_1 = road1->startPos - road1->endPos;
-				R0R1_2 = road2->endPos - road2->startPos;
-			}
-			else if (
-				road1->startJunction != nullptr &&
-				road2->endJunction != nullptr &&
-				road1->startJunction == road2->endJunction
-				)
-			{
-				R0R1_1 = road1->endPos - road1->startPos;
-				R0R1_2 = road2->startPos - road2->endPos;
-			}
-			else if (
-				road1->startJunction != nullptr &&
-				road2->startJunction != nullptr &&
-				road1->startJunction == road2->startJunction
-				)
-			{
-				R0R1_1 = road1->endPos - road1->startPos;
-				R0R1_2 = road2->endPos - road2->startPos;
-			}
-			else
-			{
-				R0R1_1 = { 0.01f, 1.0f, 0.01f };
-				R0R1_2 = { 0.01f, 1.0f, 0.01f };
-			}
-
-			float ed1 = R0R1_1.x <= 0.0f ? 180.0f : 0.0f;
-			float ed2 = R0R1_2.x <= 0.0f ? 180.0f : 0.0f;
-
-			float angleR0R1_1 = std::fmod(glm::degrees(glm::atan(-R0R1_1.z / R0R1_1.x)) + ed1 + 360.0f, 360.0f);
-			float angleR0R1_2 = std::fmod(glm::degrees(glm::atan(-R0R1_2.z / R0R1_2.x)) + ed2 + 360.0f, 360.0f);
-
-			return (angleR0R1_1 < angleR0R1_2);
-		}
-	};
-	struct Junction {
-		glm::vec3 position;
-		Can::Object* object = nullptr;
-		std::vector<Road*> connectedRoads;
-	};
-	struct End {
-		glm::vec3 position;
-		glm::vec2 rotation;
-		Can::Object* object = nullptr;
-		Road* connectedRoad = nullptr;
-	};
-
 	class GameApp;
 	class TestScene : public Can::Layer::Layer
 	{
@@ -135,7 +57,6 @@ namespace Can
 		std::vector<Can::Object*> m_JunctionGuidelines;
 		Can::Object* m_RoadGuidelinesStart = nullptr;
 		Can::Object* m_RoadGuidelinesEnd = nullptr;
-
 
 
 	public:
