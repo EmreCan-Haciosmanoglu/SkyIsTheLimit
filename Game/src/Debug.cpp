c0d9c6c755439dc52b6f6f6aa3d78ba0e15f3e65
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2020-08-12 14:00:15 +0300

    Initialization of the debug layer

---
+++ Game/src/Debug.cpp
@@ -0,0 +1,33 @@
+#include "canpch.h"
+#include "Debug.h"
+#include "GameApp.h"
+
+namespace Can
+{
+	Debug::Debug(GameApp* parent)
+		:m_Parent(parent)
+	{
+	}
+
+	void Debug::OnAttach()
+	{
+	}
+
+	void Debug::OnDetach()
+	{
+	}
+
+	void Debug::OnUpdate(Can::TimeStep ts)
+	{
+	}
+
+	void Debug::OnEvent(Can::Event::Event& event)
+	{
+	}
+
+	void Debug::OnImGuiRender()
+	{
+		ImGui::Begin("Settings");
+		ImGui::End();
+	}
+}
---
+++ Game/src/Debug.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Can.h"
+#include "imgui.h"
+
+namespace Can
+{
+
+	class GameApp;
+	class Debug : public Can::Layer::Layer
+	{
+	public:
+		Debug(GameApp* parent);
+		virtual ~Debug() = default;
+
+		virtual void OnAttach() override {};
+		virtual void OnDetach() override {};
+
+		virtual void OnUpdate(Can::TimeStep ts) override;
+		virtual void OnEvent(Can::Event::Event& event) override;
+		virtual void OnImGuiRender() override;
+	private:
+		GameApp* m_Parent;
+	};
+}
