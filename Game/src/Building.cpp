58e02c660dd2d18b9ad67a314a4b8048bfadf7bf
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2020-09-11 14:42:06 +0300

    Building class is added

---
+++ Game/src/Building.cpp
@@ -0,0 +1,22 @@
+#include "canpch.h"
+#include "Building.h"
+
+namespace Can
+{
+	Building::Building(Prefab* prefab, Road* connectedRoad, const glm::vec3& position, const glm::vec3& rotation)
+		: connectedRoad(connectedRoad)
+		, object(new Object(prefab, prefab, position, glm::vec3{ 1.0f, 1.0f, 1.0f }, rotation))
+		, position(position)
+	{
+	}
+	Building::Building(Object* object, Road* connectedRoad, const glm::vec3& position, const glm::vec3& rotation)
+		: connectedRoad(connectedRoad)
+		, object(object)
+		, position(position)
+	{
+	}
+	Building::~Building()
+	{
+		delete object;
+	}
+}
