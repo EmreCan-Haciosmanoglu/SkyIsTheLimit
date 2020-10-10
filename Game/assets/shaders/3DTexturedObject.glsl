282ced601a900d46d6f05d841b947193723f0710
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2020-10-10 23:38:41 +0300

    The lighting is fixed slightly

--- Game/assets/shaders/3DTexturedObject.glsl
+++ Game/assets/shaders/3DTexturedObject.glsl
@@ -46,18 +46,17 @@ void main()
 	vec3 lightDir = normalize(u_LightPos);
 	if(lightDir.y < 0)
 		lightDir = vec3(0.0, 0.0, 0.0);
-	lightDir.y = max(lightDir.y, 0.0);
 
 	float diff = max(dot(norm, lightDir), 0.0);
 	vec3 diffuse = (diff) * vec3(1.0, 1.0, 1.0);
 	
-	float minAL = max(0.1, elevation);
-	
+	//float minAL = max(0.1, elevation);
+	float AO = 0.2;
+
 	elevation = degrees(sin(radians(elevation*90.0)))/90.0;
 	elevation = max(0.1, elevation);
 	
-	
-	vec4 result = vec4(max( min(diffuse.x, elevation), minAL), max( min(diffuse.y, elevation), minAL), max( min(diffuse.z, elevation), minAL), 1.0);
+	vec4 result = vec4(min(diffuse.x, elevation) + AO, min(diffuse.y, elevation) + AO, min(diffuse.z, elevation) + AO, 1.0);
 
 	color = result * u_TintColor * texture(u_Textures[int(v_TextureIndex+0.1)], v_UV);
 	//color = vec4( v_UV.x, v_UV.y, 1.0f, 1.0f);
